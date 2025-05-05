370b9b41fb19d0bd61ef66ed94a767c700e00433
Deedzzz001 <208059393+Deedzzz001@users.noreply.github.com>
2025-05-05 20:48:09 +1000

    Create unmineable_logs_20250424202414.txt

---
+++ unmineable-DOGE-etchash-nbminer.bat
@@ -0,0 +1,2 @@
+nbminer.exe -a etchash -o stratum+ssl://etchash.unmineable.com:4444 -u DOGE:DMv1QsLKng1nRhVQGPKAEPjAaGofDBRmdk.unmineable_worker_mumltbfs#edi9-nc51 -log 
+pause
---
+++ unmineable-DOGE-etchash-srbminer.bat
@@ -0,0 +1,2 @@
+SRBMiner-MULTI.exe --algorithm etchash --disable-cpu --pool stratum+ssl://etchash.unmineable.com:4444 --wallet DOGE:DMv1QsLKng1nRhVQGPKAEPjAaGofDBRmdk.unmineable_worker_mumltbfs#edi9-nc51  
+
